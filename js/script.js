2dcac01753ce5d16afa01ae38b3fb9c0c4a33772
Zhantore <753gg@mail.ru>
2021-05-05 16:48:45 +0600

    parallax update

--- js/script.js
+++ js/script.js
@@ -11,14 +11,16 @@ window.onload = function () {
         const js = document.querySelector('.images-parallax__js');
         const css = document.querySelector('.images-parallax__css');
         const html = document.querySelector(".images-parallax__html");
+        const git = document.querySelector('.images-parallax__git');
+        const mongo = document.querySelector('.images-parallax__mongo');
+        const npm = document.querySelector('.images-parallax__npm');
+        const gulp = document.querySelector('.images-parallax__gulp');
+        const redux = document.querySelector('.images-parallax__redux');
+        const sass = document.querySelector('.images-parallax__sass');
+        const typescript = document.querySelector('.images-parallax__typescript');
+        const webpack = document.querySelector('.images-parallax__webpack');
 
-        const forNode = 10;
-        const forReact = 20;
-        const forJs = 40;
-        const forCss = 20;
-        const forHtml = 10;
-
-        const speed = 0.05;
+        const speed = 0.1;
 
         let positionX = 0, positionY = 0;
         let coordXprocent = 0, coordYprocent = 0;
@@ -30,12 +32,21 @@ window.onload = function () {
             positionX = positionX + (distX * speed);
             positionY = positionY + (distY * speed);
 
-            node.style.cssText = `transform: translate(${positionX / forNode}%,${positionY / forNode}%)`;
-            react.style.cssText = `transform: translate(${positionX / forReact}%,${positionY / forReact}%)`;
-            js.style.cssText = `transform: translate(${positionX / forJs}%,${positionY / forJs}%)`;
-            css.style.cssText = `transform: translate(${positionX / forCss}%,${positionY / forCss}%)`;
-            html.style.cssText = `transform: translate(${positionX / forHtml}%,${positionY / forHtml}%)`;
-        
+
+            node.style.cssText = `transform: translate(${positionX / 12}%,${positionY / 12}%)`;
+            react.style.cssText = `transform: translate(${positionX / 20}%,${positionY / 20}%)`;
+            js.style.cssText = `transform: translate(${positionX / 40}%,${positionY / 40}%)`;
+            css.style.cssText = `transform: translate(${positionX / 9}%,${positionY / 9}%)`;
+            html.style.cssText = `transform: translate(${positionX / 15}%,${positionY / 15}%)`;
+            git.style.cssText = `transform: translate(${positionX / 9}%,${positionY / 9}%)`;
+            mongo.style.cssText = `transform: translate(${positionX / 7}%,${positionY / 7}%)`;
+            typescript.style.cssText = `transform: translate(${positionX / 4}%,${positionY / 4}%)`;
+            sass.style.cssText = `transform: translate(${positionX / 6}%,${positionY / 6}%)`;
+            redux.style.cssText = `transform: translate(${positionX / 3}%,${positionY / 3}%)`;
+            webpack.style.cssText = `transform: translate(${positionX / 5}%,${positionY / 5}%)`;
+            gulp.style.cssText = `transform: translate(${positionX / 7}%,${positionY / 7}%)`;
+            npm.style.cssText = `transform: translate(${positionX / 4}%,${positionY / 4}%)`;
+
             requestAnimationFrame(setMouseParallaxStyle);
         }
         setMouseParallaxStyle();
@@ -68,10 +79,19 @@ window.onload = function () {
         function setParallaxItemStyle(scrollTopProcent){
             content.style.cssText = `transform: translate(0%,-${scrollTopProcent / 9}%)`;
             node.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 3}%)`;
-            js.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 9}%)`;
-            react.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 6}%)`;
-            css.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 6}%)`;
+            js.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 5}%)`;
+            react.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 4}%)`;
+            css.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 1}%)`;
             html.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 3}%)`;
+
+            git.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 2}%)`;
+            mongo.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 5}%)`;
+            typescript.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 4}%)`;
+            sass.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 3}%)`;
+            redux.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 5}%)`;
+            webpack.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 2}%)`;
+            gulp.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 3}%)`;
+            npm.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 2}%)`;
         }
     }
 }
